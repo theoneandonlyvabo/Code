9b6fb5105d60a5e11a35e4cec46b224f47310749
Airel Adrivano <aireladrivano196@gmail.com>
2025-12-11 22:54:31 +0700

    11.12.2025

--- Go/go-web-scraper/go.mod
+++ Go/go-web-scraper/go.mod
@@ -1,3 +1,10 @@
 module github.com/aireladrivano/go-web-scraper
 
 go 1.25.5
+
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/go-chi/cors v1.2.2
+)
--- Go/go-web-scraper/main.go
+++ Go/go-web-scraper/main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Debug Successful!")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Port not found")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port:", portString)
 }
